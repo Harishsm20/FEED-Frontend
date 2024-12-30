546c921ece368eb19fd283ce0e66c46439ea9e71
Harishsm20 <harishsm865@gmail.com>
2024-12-30 20:50:02 +0530

    Tokens issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { CustomSlider } from '../components'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { CustomSlider } from '../components';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the token is available in localStorage
+    const token = Cookies.get('jwt');
+    console.log(token);
+    
+    // if (token) {
+    //   // Save the token in a cookie
+    //   Cookies.set('jwt', token, {
+    //     expires: 1, // 1 day
+    //     secure: process.env.NODE_ENV === 'production', // Only secure in production
+    //     sameSite: 'Strict',
+    //   });
+
+    //   console.log('Token saved in cookies');
+    // }
+    if(!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <>
-      <CustomSlider/>
+      <CustomSlider />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
